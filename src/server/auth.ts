50d5ea1858ac1a18ced876f79400056eeb7d764e
Ziek <zieksanchez3@gmail.com>
2023-03-01 01:10:21 -0600

    auth changes

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,7 +4,7 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import EmailProvider from "next-auth/providers/email";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "../env/server.mjs";
@@ -60,6 +60,10 @@ export const authOptions: NextAuthOptions = {
           response_type: "code",
         },
       },
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
     }),
     /**
      * ...add more providers here
